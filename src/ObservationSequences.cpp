20c17fbe4053f7f00415a91e3961d05b58498f4d
adityaraghavan <adiraghavan.11@gmail.com>
2016-11-30 00:27:57 -0800

    Modified training data structure and added benign data

--- src/ObservationSequences.cpp
+++ src/ObservationSequences.cpp
@@ -4,7 +4,6 @@
 #include <algorithm>
 #include "boost\filesystem.hpp"
 
-
 using namespace std;
 namespace bst = boost::filesystem;
 
@@ -18,6 +17,7 @@ ObservationSequences::ObservationSequences(std::string folderName)
 	noOfFiles = 0;
 	noOfTrainingFiles = 0;
 	noOfScoringFiles = 0;
+	this->parentFolderPath = "D:/Aditya/CS_266/Project/Dataset/";
 	this->malwareFamilyName = folderName;
 }
 
@@ -35,8 +35,7 @@ int main()
 
 void ObservationSequences::getFileList() {
 
-	string folderName = "D:/Aditya/CS_266/Project/Dataset/" + this->malwareFamilyName + "/";
-	cout << folderName << endl;
+	string folderName = this->parentFolderPath + this->malwareFamilyName + "/";
 	bst::path p(folderName);
 	for (auto i = bst::directory_iterator(p); i != bst::directory_iterator(); i++)
 	{
@@ -52,11 +51,51 @@ void ObservationSequences::getFileStream()
 {
 
 	this->noOfFiles = this->fileNameList.size();
-	for (int index = 0; index < this->noOfFiles; index++) 
+	this->noOfTrainingFiles = this->noOfFiles * 0.6 + 1;
+
+	this->getTrainingData();
+	this->getScoringData();
+}
+
+void ObservationSequences::getTrainingData() 
+{
+	int lastIndex = this->noOfTrainingFiles;
+	for (int index = 0; index < lastIndex; index++)
 	{
 		vector<int> tempFileStream;
-		vector<string> opCodeStream;
+		
+		string tempFileName = this->fileNameList.at(index);
+		ifstream tempReadFile;
+		tempReadFile.open(tempFileName);
 
+		string line;
+		while (getline(tempReadFile, line))
+		{
+			int opCodeIndex = find(this->distinctOpCodesList.begin(), this->distinctOpCodesList.end(), line) - this->distinctOpCodesList.begin();
+			int endIndex = this->distinctOpCodesList.size();
+
+			if (opCodeIndex != endIndex)
+			{
+				this->trainingData.push_back(opCodeIndex);
+			}
+			else
+			{
+				this->distinctOpCodesList.push_back(line);
+				int newOpCodeIndex = this->distinctOpCodesList.size() - 1;
+
+				this->trainingData.push_back(newOpCodeIndex);
+			}
+		}
+	}
+}
+
+void ObservationSequences::getScoringData()
+{
+	int firstIndex = this->noOfTrainingFiles;
+	for (int index = firstIndex; index < this->noOfFiles; index++)
+	{
+		vector<int> tempFileStream;
+		
 		string tempFileName = this->fileNameList.at(index);
 		ifstream tempReadFile;
 		tempReadFile.open(tempFileName);
@@ -74,13 +113,56 @@ void ObservationSequences::getFileStream()
 			else
 			{
 				this->distinctOpCodesList.push_back(line);
-				int newOpCodeIndex = this->distinctOpCodesList.size()-1;
+				int newOpCodeIndex = this->distinctOpCodesList.size() - 1;
 
 				tempFileStream.push_back(newOpCodeIndex);
 			}
-			opCodeStream.push_back(line);
 		}
-		this->obsSequenceList.push_back(tempFileStream);		
+		this->scoringData.push_back(tempFileStream);
 	}
-	cout << this->distinctOpCodesList.size();
+
+	addBenignData();
+	this->noOfScoringFiles = this->scoringData.size();
+}
+
+void ObservationSequences::addBenignData() {
+
+	string folderName = this->parentFolderPath + "/Benign/";
+	bst::path p(folderName);
+	for (auto i = bst::directory_iterator(p); i != bst::directory_iterator(); i++)
+	{
+		if (!bst::is_directory(i->path()))
+		{
+			string fullFileName = folderName + i->path().filename().string();
+			getBenignData(fullFileName);
+		}
+	}
+}
+
+void ObservationSequences::getBenignData(string fullFileName) {
+
+	vector<int> tempFileStream;
+
+	ifstream tempReadFile;
+	tempReadFile.open(fullFileName);
+
+	string line;
+	while (getline(tempReadFile, line))
+	{
+		int opCodeIndex = find(this->distinctOpCodesList.begin(), this->distinctOpCodesList.end(), line) - this->distinctOpCodesList.begin();
+		int endIndex = this->distinctOpCodesList.size();
+
+		if (opCodeIndex != endIndex)
+		{
+			tempFileStream.push_back(opCodeIndex);
+		}
+		else
+		{
+			this->distinctOpCodesList.push_back(line);
+			int newOpCodeIndex = this->distinctOpCodesList.size() - 1;
+
+			tempFileStream.push_back(newOpCodeIndex);
+		}
+	}
+	this->scoringData.push_back(tempFileStream);
 }
--- src/ObservationSequences.h
+++ src/ObservationSequences.h
@@ -7,17 +7,25 @@ class ObservationSequences
 
 public:
 	std::string malwareFamilyName;
+	std::string parentFolderPath;
 	~ObservationSequences();
 	ObservationSequences(std::string);
+	
 	void getFileList();
 	void getFileStream();
+
+	void getTrainingData();
+	void getScoringData();
+	void addBenignData();
+	void getBenignData(std::string);
 
 	int noOfFiles;
 	int noOfTrainingFiles;
 	int noOfScoringFiles;
 
 private:
-	std::vector<std::vector<int>> obsSequenceList;
+	std::vector<int> trainingData;
+	std::vector<std::vector<int>> scoringData;
 	std::vector<std::string> distinctOpCodesList;
 	
 	std::vector<std::string> fileNameList;
